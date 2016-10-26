2af64971d57d92a0842a8aed88d81461e10414c0
blerimcazimi <blerimcazimi@Blerims-MacBook-Pro.local>
2016-10-26 04:24:27 +0200

    Binary search

---
+++ BinarySearch.php
@@ -0,0 +1,27 @@
+<?php
+function binarySearch($key, $array)
+{
+    
+    $lo = 0;
+    $hi = count($array) - 1;
+
+    while($lo <= $hi)
+    {
+
+        $mid = floor($lo + ($hi - $lo) / 2);
+
+        if ($key < $array[$mid]) 
+        {
+            $hi = $mid - 1;
+        } else if ($key > $array[$mid]) 
+        { 
+            $lo = $mid + 1;
+        } else {
+            return $mid;
+        }
+
+    }
+
+    return -1;
+    
+}
